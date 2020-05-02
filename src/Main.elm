b7be2e7fa44551afd6b0926f9e8ae363dcf49157
Matias Klemola <matias.klemola@gmail.com>
2020-05-02 15:54:12 +0300

    Replace debug view Html with elm-ui things

--- src/Main.elm
+++ src/Main.elm
@@ -3,13 +3,13 @@ module Main exposing (main)
 import Browser
 import Collage.Render exposing (svg)
 import Config
+import Element
+import Element.Background as Background
 import Game exposing (Msg(..))
-import Graphics
-import Html exposing (Html, div)
-import Html.Attributes exposing (style)
-import SharedState exposing (Mode(..), SharedState)
+import Html exposing (Html)
+import SharedState exposing (SharedState, SimulationState(..))
 import Time
-import UI exposing (Msg(..))
+import UI exposing (Msg(..), colors)
 
 
 main : Program () Model Msg
@@ -23,7 +23,7 @@ subs model =
         ( slowTickSpeed, fastTickSpeed ) =
             SharedState.simulationSpeedValues model.sharedState.simulationSpeed
     in
-    case model.sharedState.mode of
+    case model.sharedState.simulationState of
         Simulation ->
             Sub.batch
                 [ Time.every slowTickSpeed SlowTick
@@ -90,7 +90,7 @@ update msg model =
                 updatedSharedState =
                     case uiMsg of
                         ToggleSimulation ->
-                            { ss | mode = SharedState.nextMode ss.mode }
+                            { ss | simulationState = SharedState.nextSimulationState ss.simulationState }
 
                         SetSimulationSpeed speed ->
                             { ss | simulationSpeed = speed }
@@ -101,20 +101,23 @@ update msg model =
 view : Model -> Html Msg
 view model =
     let
-        padding =
-            String.fromFloat (Config.tileSize / 2) ++ "px"
+        paddingAmount =
+            floor (Config.tileSize / 2)
 
-        minHeight =
-            "calc(100vh - " ++ String.fromFloat Config.tileSize ++ "px)"
+        layout =
+            Element.layout
+                [ Background.color colors.mainBackground
+                ]
+                (Element.column
+                    [ Element.centerX
+                    , Element.centerY
+                    , Element.padding paddingAmount
+                    ]
+                    [ Game.view model.game
+                        |> svg
+                        |> Element.html
+                    , UI.debug model.game model.sharedState
+                    ]
+                )
     in
-    div
-        [ style "display" "flex"
-        , style "background-color" Graphics.backgroundCss
-        , style "flex-direction" "column"
-        , style "align-items" "center"
-        , style "padding" padding
-        , style "min-height" minHeight
-        ]
-        [ Game.view model.game |> svg
-        , Html.map UIMsg (UI.view model.game model.sharedState)
-        ]
+    Html.map UIMsg layout
