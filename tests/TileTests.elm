f39b7c26b11a78dbf02f9a490c2c4746de1529d2
Matias Klemola <matias.klemola@gmail.com>
2020-07-04 16:40:39 +0300

    Add tests for Tile module

---
+++ tests/TileTests.elm
@@ -0,0 +1,154 @@
+module TileTests exposing (suite)
+
+import Direction exposing (Direction(..), Orientation(..))
+import Expect
+import Test exposing (..)
+import Tile
+    exposing
+        ( CurveKind(..)
+        , IntersectionControl(..)
+        , IntersectionShape(..)
+        , RoadKind(..)
+        , Tile(..)
+        , TrafficDirection(..)
+        )
+
+
+suite : Test
+suite =
+    describe "Tile"
+        [ describe ".connected"
+            [ test "connects pair of horizontal road pieces"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Regular Horizontal) Both
+
+                        tileB =
+                            TwoLaneRoad (Regular Horizontal) Both
+                    in
+                    Tile.connected Right tileA tileB
+                        |> Expect.true "Expected tiles to connect."
+                )
+            , test "does not connect pair of horizontal road pieces in vertical direction"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Regular Horizontal) Both
+
+                        tileB =
+                            TwoLaneRoad (Regular Horizontal) Both
+                    in
+                    Tile.connected Up tileA tileB
+                        |> Expect.false "Expected tiles to NOT connect."
+                )
+            , test "connects a intersection with plain road"
+                (\_ ->
+                    let
+                        tileA =
+                            Intersection (Yield Vertical) (T Right)
+
+                        tileB =
+                            TwoLaneRoad (Regular Horizontal) Both
+                    in
+                    Tile.connected Right tileA tileB
+                        |> Expect.true "Expected tiles to connect."
+                )
+            , test "connects a curved road from entry point A"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Curve TopLeft) Both
+
+                        tileB =
+                            TwoLaneRoad (Regular Vertical) Both
+                    in
+                    Tile.connected Down tileA tileB
+                        |> Expect.true "Expected tiles to connect."
+                )
+            , test "connects a curved road from entry point B"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Curve TopLeft) Both
+
+                        tileB =
+                            TwoLaneRoad (Regular Horizontal) Both
+                    in
+                    Tile.connected Right tileA tileB
+                        |> Expect.true "Expected tiles to connect."
+                )
+            , test "connects a dead end road piece from entry"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Regular Horizontal) Both
+
+                        tileB =
+                            TwoLaneRoad (Deadend Right) Both
+                    in
+                    Tile.connected Right tileA tileB
+                        |> Expect.true "Expected tiles to connect."
+                )
+            , test "does not connect a dead end road piece closed side"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Deadend Right) Both
+
+                        tileB =
+                            TwoLaneRoad (Regular Horizontal) Both
+                    in
+                    Tile.connected Right tileA tileB
+                        |> Expect.false "Expected tiles to NOT connect."
+                )
+            , test "connects two pieces of one-way road"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Regular Horizontal) OneWay
+
+                        tileB =
+                            TwoLaneRoad (Regular Horizontal) OneWay
+                    in
+                    Tile.connected Right tileA tileB
+                        |> Expect.true "Expected tiles to connect."
+                )
+            , test "does not connect a one-way road pieces with different orientations"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Regular Horizontal) OneWay
+
+                        tileB =
+                            TwoLaneRoad (Regular Vertical) OneWay
+                    in
+                    Tile.connected Left tileA tileB
+                        |> Expect.false "Expected tiles to NOT connect."
+                )
+            , test "connects a one-way road piece with an intersection by entry point"
+                (\_ ->
+                    let
+                        tileA =
+                            Intersection (Yield Vertical) Crossroads
+
+                        tileB =
+                            TwoLaneRoad (Regular Horizontal) OneWay
+                    in
+                    Tile.connected Right tileA tileB
+                        |> Expect.true "Expected tiles to connect."
+                )
+            , test "does not connect a one-way road piece with an intersection by exit point"
+                (\_ ->
+                    let
+                        tileA =
+                            TwoLaneRoad (Regular Horizontal) OneWay
+
+                        tileB =
+                            Intersection (Yield Vertical) (T Left)
+                    in
+                    Tile.connected Left tileA tileB
+                        |> Expect.false "Expected tiles to NOT connect."
+                )
+            ]
+        ]
