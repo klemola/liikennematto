727db135a4cb88a51c84cac8e8a3588c26fe93a1
Matias Klemola <matias.klemola@gmail.com>
2021-05-02 14:10:47 +0300

    Add quadtree benchmark

--- benchmarks/RoundBenchmark.elm
+++ benchmarks/RoundBenchmark.elm
@@ -2,9 +2,14 @@ module RoundBenchmark exposing (main, suite)
 
 import Benchmark exposing (..)
 import Benchmark.Runner exposing (BenchmarkProgram, program)
+import Board
+import Car
+import Config
 import Dict
-import Round
+import QuadTree
+import Round exposing (Round)
 import Rounds
+import Set
 
 
 suite : Benchmark
@@ -22,7 +27,7 @@ suite =
             ]
         , describe "Large map"
             [ -- Due to random generation, the amount of cars in play might be smaller (can't place cars too close to another)
-              [ 5, 25, 50, 100 ]
+              [ 5, 50, 100 ]
                 |> List.map Rounds.largeWorldSetup
                 |> List.map
                     (\round ->
@@ -31,8 +36,44 @@ suite =
                         )
                     )
                 |> scale "cars amount - not optimized"
+            , [ 5, 50, 100 ]
+                |> List.map Rounds.largeWorldSetup
+                |> List.map chooseOtherCarsWithQuadtree
+                |> List.map
+                    (\round ->
+                        ( Dict.size round.world.cars |> String.fromInt
+                        , \_ -> Round.play round
+                        )
+                    )
+                |> scale "cars amount - with quadtree"
             ]
         ]
+
+
+chooseOtherCarsWithQuadtree : Round -> Round
+chooseOtherCarsWithQuadtree round =
+    let
+        carBB =
+            Car.boundingBox round.activeCar
+
+        otherCarBBs =
+            round.otherCars
+                |> List.map
+                    (\car ->
+                        { id = car.id
+                        , position = car.position
+                        , boundingBox = Car.boundingBox car
+                        }
+                    )
+
+        matchesSet =
+            -- QuadTree -> choose neighbor cars -> Set of ids
+            QuadTree.init Board.boundingBox 2
+                |> QuadTree.insertList otherCarBBs
+                |> QuadTree.neighborsWithin Config.tileSizeInMeters carBB
+                |> List.foldl (\bounded acc -> Set.insert bounded.id acc) Set.empty
+    in
+    { round | otherCars = List.filter (\car -> Set.member car.id matchesSet) round.otherCars }
 
 
 main : BenchmarkProgram
