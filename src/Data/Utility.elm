fcfbdf6ba98f7578df184f187c424db4ff2f720d
Matias Klemola <matias.klemola@gmail.com>
2022-03-20 21:50:00 +0200

    Add a test for lot entry bitmask

--- src/Data/Utility.elm
+++ src/Data/Utility.elm
@@ -1,18 +1,45 @@
 module Data.Utility exposing (..)
 
 import Model.Cell as Cell
+import Model.Entity exposing (Id)
+import Model.Geometry exposing (OrthogonalDirection)
 import Model.Tilemap as Tilemap exposing (Tilemap)
 import Model.World as World exposing (World)
 import Simulation.Infrastructure as Infrastructure
 
 
-tilemapFromCoordinates : List ( Int, Int ) -> Tilemap
-tilemapFromCoordinates cellCoordinates =
+type alias AnchorDef =
+    { lotId : Id
+    , anchorDirection : OrthogonalDirection
+    , cellCoordinates : Cell.CellCoordinates
+    }
+
+
+tilemapFromCoordinates : List ( Int, Int ) -> List AnchorDef -> Tilemap
+tilemapFromCoordinates cellCoordinates anchorDefs =
     let
         cells =
             List.filterMap Cell.fromCoordinates cellCoordinates
+
+        withCells =
+            Tilemap.fromCells cells
     in
-    Tilemap.fromCells cells
+    addAnchors withCells anchorDefs
+
+
+addAnchors : Tilemap -> List AnchorDef -> Tilemap
+addAnchors tilemap anchorDefs =
+    List.foldl
+        (\{ cellCoordinates, lotId, anchorDirection } nextTilemap ->
+            case Cell.fromCoordinates cellCoordinates of
+                Just cell ->
+                    Tilemap.addAnchor cell lotId anchorDirection nextTilemap
+
+                Nothing ->
+                    nextTilemap
+        )
+        tilemap
+        anchorDefs
 
 
 worldFromTilemap : Tilemap -> World
