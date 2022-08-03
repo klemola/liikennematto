d4f7d3f1f62a1ee1dec57ef1dd7722b53c0c87d7
Matias Klemola <matias.klemola@gmail.com>
2022-08-03 14:16:17 +0300

    Update RenderFixture to work with the new render debug module

--- devtools/RenderFixture.elm
+++ devtools/RenderFixture.elm
@@ -1,9 +1,11 @@
 module RenderFixture exposing (main)
 
 import Data.RuleSetups as RuleSetups
+import Element
 import Html exposing (Html)
 import Model.RenderCache as RenderCache
 import Render
+import Render.Debug
 
 
 main : Html msg
@@ -11,8 +13,31 @@ main =
     let
         world =
             RuleSetups.collisionSetupPathsIntersect.world
+
+        cache =
+            RenderCache.new world
+
+        renderWidth =
+            floor cache.tilemapWidthPixels
+
+        renderHeight =
+            floor cache.tilemapHeightPixels
+
+        renderDebug =
+            Render.Debug.view
+                world
+                cache
+                { showRoadNetwork = False, showCarDebugVisuals = True }
+                |> Element.html
     in
-    Render.view
-        world
-        (RenderCache.new world)
-        { showRoadNetwork = False, showCarDebugVisuals = True }
+    Render.view world cache
+        |> Element.html
+        |> Element.el
+            [ Element.width (Element.px renderWidth)
+            , Element.height (Element.px renderHeight)
+            , Element.inFront renderDebug
+            ]
+        |> Element.layout
+            [ Element.width Element.fill
+            , Element.height Element.fill
+            ]
