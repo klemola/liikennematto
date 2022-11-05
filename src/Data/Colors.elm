560ed24b48ce955fa7f31b364d54b928acc5ccb3
Matias Klemola <matias.klemola@gmail.com>
2022-11-05 12:41:25 +0200

    Refresh the color palette

--- src/Data/Colors.elm
+++ src/Data/Colors.elm
@@ -16,6 +16,8 @@ module Data.Colors exposing
     , gray5CSS
     , gray6
     , gray6CSS
+    , gray7
+    , gray7CSS
     , lightBlue
     , lightBlueCSS
     , lightBrown
@@ -54,7 +56,7 @@ withAlpha alpha color =
 
 gray1 : Color.Color
 gray1 =
-    Color.rgb255 48 37 37
+    Color.rgb255 61 52 52
 
 
 gray1CSS : String
@@ -64,7 +66,7 @@ gray1CSS =
 
 gray2 : Color.Color
 gray2 =
-    Color.rgb255 120 94 94
+    Color.rgb255 118 101 101
 
 
 gray2CSS : String
@@ -94,7 +96,7 @@ gray4CSS =
 
 gray5 : Color.Color
 gray5 =
-    Color.rgb255 240 240 221
+    Color.rgb255 217 217 181
 
 
 gray5CSS : String
@@ -104,12 +106,22 @@ gray5CSS =
 
 gray6 : Color.Color
 gray6 =
-    Color.rgb255 249 249 233
+    Color.rgb255 240 240 221
 
 
 gray6CSS : String
 gray6CSS =
     Color.toCssString gray6
+
+
+gray7 : Color.Color
+gray7 =
+    Color.rgb255 249 249 233
+
+
+gray7CSS : String
+gray7CSS =
+    Color.toCssString gray7
 
 
 red : Color.Color
@@ -122,14 +134,34 @@ redCSS =
     Color.toCssString red
 
 
+redDarker : Color.Color
+redDarker =
+    Color.rgb255 188 47 47
+
+
+redDarkerCSS : String
+redDarkerCSS =
+    Color.toCssString red
+
+
 orange : Color.Color
 orange =
-    Color.rgb255 235 145 81
+    Color.rgb255 229 140 77
 
 
 orangeCSS : String
 orangeCSS =
     Color.toCssString orange
+
+
+orangeDarker : Color.Color
+orangeDarker =
+    Color.rgb255 233 134 63
+
+
+orangeDarkerCSS : String
+orangeDarkerCSS =
+    Color.toCssString orangeDarker
 
 
 yellow : Color.Color
@@ -142,6 +174,16 @@ yellowCSS =
     Color.toCssString yellow
 
 
+yellowDarker : Color.Color
+yellowDarker =
+    Color.rgb255 231 200 45
+
+
+yellowDarkerCSS : String
+yellowDarkerCSS =
+    Color.toCssString yellowDarker
+
+
 darkBrown : Color.Color
 darkBrown =
     Color.rgb255 150 111 64
@@ -152,14 +194,34 @@ darkBrownCSS =
     Color.toCssString darkBrown
 
 
+darkBrownDarker : Color.Color
+darkBrownDarker =
+    Color.rgb255 134 100 60
+
+
+darkBrownDarkerCSS : String
+darkBrownDarkerCSS =
+    Color.toCssString darkBrownDarker
+
+
 lightBrown : Color.Color
 lightBrown =
-    Color.rgb255 216 164 106
+    Color.rgb255 203 164 118
 
 
 lightBrownCSS : String
 lightBrownCSS =
     Color.toCssString lightBrown
+
+
+lightBrownDarker : Color.Color
+lightBrownDarker =
+    Color.rgb255 191 146 95
+
+
+lightBrownDarkerCSS : String
+lightBrownDarkerCSS =
+    Color.toCssString lightBrownDarker
 
 
 lightGreen : Color.Color
@@ -172,6 +234,16 @@ lightGreenCSS =
     Color.toCssString lightGreen
 
 
+lightGreenDarker : Color.Color
+lightGreenDarker =
+    Color.rgb255 92 152 78
+
+
+lightGreenDarkerCSS : String
+lightGreenDarkerCSS =
+    Color.toCssString lightGreenDarker
+
+
 darkGreen : Color.Color
 darkGreen =
     Color.rgb255 76 137 67
@@ -180,6 +252,16 @@ darkGreen =
 darkGreenCSS : String
 darkGreenCSS =
     Color.toCssString darkGreen
+
+
+darkGreenDarker : Color.Color
+darkGreenDarker =
+    Color.rgb255 61 110 54
+
+
+darkGreenDarkerCSS : String
+darkGreenDarkerCSS =
+    Color.toCssString darkGreenDarker
 
 
 yellowGreen : Color.Color
@@ -192,6 +274,16 @@ yellowGreenCSS =
     Color.toCssString yellowGreen
 
 
+yellowGreenDarker : Color.Color
+yellowGreenDarker =
+    Color.rgb255 130 155 59
+
+
+yellowGreenDarkerCSS : String
+yellowGreenDarkerCSS =
+    Color.toCssString yellowGreenDarker
+
+
 darkBlue : Color.Color
 darkBlue =
     Color.rgb255 49 140 231
@@ -202,6 +294,16 @@ darkBlueCSS =
     Color.toCssString darkBlue
 
 
+darkBlueDarker : Color.Color
+darkBlueDarker =
+    Color.rgb255 25 120 215
+
+
+darkBlueDarkerCSS : String
+darkBlueDarkerCSS =
+    Color.toCssString darkBlueDarker
+
+
 lightBlue : Color.Color
 lightBlue =
     Color.rgb255 136 176 221
@@ -210,3 +312,13 @@ lightBlue =
 lightBlueCSS : String
 lightBlueCSS =
     Color.toCssString lightBlue
+
+
+lightBlueDarker : Color.Color
+lightBlueDarker =
+    Color.rgb255 104 155 212
+
+
+lightBlueDarkerCSS : String
+lightBlueDarkerCSS =
+    Color.toCssString lightBlueDarker
