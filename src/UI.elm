d84ec854d18a618d0e81c2cb0721d12e46ee1457
Matias Klemola <matias.klemola@gmail.com>
2025-09-15 09:03:47 +0300

    Improve menu layout

--- src/UI.elm
+++ src/UI.elm
@@ -19,13 +19,10 @@ import Model.Debug exposing (DebugLayerKind(..))
 import Model.Liikennematto exposing (Liikennematto)
 import Model.World exposing (World)
 import Render.Conversion exposing (PixelsToMetersRatio)
-import Tilemap.Cell exposing (bottomLeftCorner)
 import UI.Button exposing (iconButton, iconWithTextButton)
 import UI.Core
     exposing
-        ( borderRadiusPanel
-        , borderSize
-        , colorBorder
+        ( borderSize
         , colorMainBackground
         , colorRenderEdge
         , containerId
@@ -80,17 +77,13 @@ touchLayoutOptions =
     Element.noHover :: baseLayoutOptions
 
 
-borderRadiusRender =
-    10
-
-
-colorMenuBackground : Element.Color
-colorMenuBackground =
+menuBackgroundColor : Element.Color
+menuBackgroundColor =
     Element.rgb255 190 198 213
 
 
-colorMenuBackgroundAlt : Element.Color
-colorMenuBackgroundAlt =
+menuBackgroundColorAlt : Element.Color
+menuBackgroundColorAlt =
     Element.rgb255 177 187 205
 
 
@@ -102,9 +95,21 @@ uiButtonTxtColor =
     Element.rgb255 44 53 68
 
 
-borderRadiusMenu : Int
-borderRadiusMenu =
+menuButtonSpacingPx =
+    6
+
+
+borderRadiusMenuPx : Int
+borderRadiusMenuPx =
     8
+
+
+borderRadiusRenderPx =
+    10
+
+
+fontSizeSectionHeading =
+    14
 
 
 subscriptions : UI -> Sub Msg
@@ -312,7 +317,7 @@ renderWrapper { renderCache, world, screen } render debugLayers model =
             , Element.clip
             , verticalAlignment
             , Border.solid
-            , Border.rounded borderRadiusRender
+            , Border.rounded borderRadiusRenderPx
             , Border.width borderSize
             , Border.color colorRenderEdge
             , Element.inFront
@@ -341,7 +346,7 @@ gameControls simulationActive =
             [ Element.padding whitespaceTight
             , Element.spacing whitespaceTight
             , Element.alignBottom
-            , Background.color colorMenuBackground
+            , Background.color menuBackgroundColor
             ]
             (simulationControl simulationActive)
         ]
@@ -367,14 +372,40 @@ simulationControl simulationActive =
 
 menu : UI -> Element Msg
 menu model =
+    let
+        contentSpacingPx =
+            24
+
+        menuHiddenContentAttrs =
+            [ Element.height (Element.px 0)
+            ]
+
+        menuVisibleContentAttrs =
+            [ Element.height Element.shrink
+            , Element.paddingEach
+                { top = 32
+                , right = 10
+                , bottom = 16
+                , left = 10
+                }
+            , Border.widthEach
+                { top = 1
+                , left = 0
+                , bottom = 0
+                , right = 0
+                }
+            ]
+    in
     Element.column
         [ Element.alignTop
         , Element.alignRight
-        , Element.width (Element.px 220)
+        , Element.width (Element.px 200)
+        , Element.height Element.shrink
+        , Element.clipY
         , Element.moveDown 10
         , Element.moveLeft scrollbarAwareOffsetF
-        , Background.color colorMenuBackground
-        , Border.rounded borderRadiusMenu
+        , Background.color menuBackgroundColor
+        , Border.rounded borderRadiusMenuPx
         , Border.color borderColor
         , Border.width 2
         ]
@@ -382,23 +413,17 @@ menu model =
             [ Element.spacing whitespaceTight
             , Element.padding whitespaceRegular
             , Element.width Element.fill
-            , Background.color colorMenuBackgroundAlt
-            , Border.roundEach
-                { topLeft = borderRadiusMenu
-                , topRight = borderRadiusMenu
-                , bottomLeft =
-                    if model.showMenu then
-                        0
+            , Background.color menuBackgroundColorAlt
+            , if model.showMenu then
+                Border.roundEach
+                    { topLeft = borderRadiusMenuPx
+                    , topRight = borderRadiusMenuPx
+                    , bottomLeft = 0
+                    , bottomRight = 0
+                    }
 
-                    else
-                        borderRadiusMenu
-                , bottomRight =
-                    if model.showMenu then
-                        0
-
-                    else
-                        borderRadiusMenu
-                }
+              else
+                Border.rounded borderRadiusMenuPx
             ]
             [ Element.el [ Element.alignRight ]
                 (iconButton
@@ -410,38 +435,26 @@ menu model =
                 )
             ]
         , Element.column
-            [ Element.height
-                (if model.showMenu then
-                    Element.px 420
-
-                 else
-                    Element.px 0
-                )
-            , Element.width Element.fill
-            , Element.clipY
-            , Element.paddingXY 10 32
-            , Element.spacing 24
-            , Border.color borderColor
-            , Border.widthEach
-                { top =
-                    if model.showMenu then
-                        1
-
-                    else
-                        0
-                , left = 0
-                , bottom = 0
-                , right = 0
-                }
-            , Border.roundEach
+            ([ Element.width Element.fill
+             , Element.spacing contentSpacingPx
+             , Border.color borderColor
+             , Border.roundEach
                 { topLeft = 0
                 , topRight = 0
-                , bottomLeft = borderRadiusMenu
-                , bottomRight = borderRadiusMenu
+                , bottomLeft = borderRadiusMenuPx
+                , bottomRight = borderRadiusMenuPx
                 }
-            ]
+             ]
+                ++ (if model.showMenu then
+                        menuVisibleContentAttrs
+
+                    else
+                        menuHiddenContentAttrs
+                   )
+            )
             [ Element.column
                 [ Element.width Element.fill
+                , Element.spacing menuButtonSpacingPx
                 ]
                 [ iconWithTextButton
                     { onPress = Trigger NewGame
@@ -452,16 +465,9 @@ menu model =
                     Icons.iconNewGame
                 ]
             , Element.column
-                [ Element.width Element.fill
-                , Element.paddingXY 8 6
-                , Element.spacing 6
-                , Background.color colorMenuBackgroundAlt
-                , Border.rounded borderRadiusMenu
-                , Border.color colorBorder
-                , Border.width 1
-                ]
+                menuSectionAttrs
                 [ Element.el
-                    [ Font.size 14
+                    [ Font.size fontSizeSectionHeading
                     , Font.bold
                     , Font.color uiButtonTxtColor
                     ]
@@ -489,14 +495,7 @@ menu model =
                     Icons.iconGraphDebug
                 ]
             , Element.column
-                [ Element.width Element.fill
-                , Element.paddingXY 8 6
-                , Element.spacing 6
-                , Background.color colorMenuBackgroundAlt
-                , Border.rounded borderRadiusMenu
-                , Border.color colorBorder
-                , Border.width 1
-                ]
+                menuSectionAttrs
                 [ iconWithTextButton
                     { onPress = Trigger SpawnCar
                     , selected = False
@@ -507,6 +506,18 @@ menu model =
                 ]
             ]
         ]
+
+
+menuSectionAttrs : List (Element.Attribute msg)
+menuSectionAttrs =
+    [ Element.width Element.fill
+    , Element.paddingXY 8 6
+    , Element.spacing menuButtonSpacingPx
+    , Background.color menuBackgroundColorAlt
+    , Border.rounded borderRadiusMenuPx
+    , Border.color borderColor
+    , Border.width 1
+    ]
 
 
 devMenu : Liikennematto -> UI -> Element Msg
